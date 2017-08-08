5c95a393eefac684df8c7d312e75164bf7856d90
Fernando Francisco de Oliveira <fernando@oliveira.eti.br>
2017-08-08 09:12:58 -0300

    Configured for Ionic Deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import {Deploy} from '@ionic/cloud-angular';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +26,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+	Deploy,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
